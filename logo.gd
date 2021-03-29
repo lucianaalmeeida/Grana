72b5e7b1731c34021843613ab9b3114a072d80ae
Luciana Almeida <41968007+mlucianaap@users.noreply.github.com>
2021-03-29 13:55:52 -0300

    Delete logo.gd

--- logo.gd
+++
@@ -1,9 +0,0 @@
-extends CanvasLayer
-
-#APRESENTAR A ANIMAÇÃO DA LOGO
-func _ready():
-	get_node("Anim").play("Fade")
-
-#FUNÇÃO PARA MUDAR PARA A CENA DO MENU
-func mudar_scene():
-	get_tree().change_scene("res://scenes/menu.tscn")
