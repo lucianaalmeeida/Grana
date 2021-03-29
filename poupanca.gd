2e4eb0e1e4ee02b8d7f551347eaef9d60a556c4b
Luciana Almeida <41968007+mlucianaap@users.noreply.github.com>
2021-03-29 13:56:30 -0300

    Delete poupanca.gd

--- poupanca.gd
+++
@@ -1,8 +0,0 @@
-extends Label
-
-func _ready():
-	set_text(str(game.poupanca)) #SETA O VALOR DA POUPANÇA NO LABEL
-	game.connect("poupanca_mudou", self, "_on_poupanca_mudou") #QUANDO O SINAL poupanca_mudou FOR ACIONADO A FUNÇÃO _on_poupanca_mudou É EXECUTADA
-
-func _on_poupanca_mudou():
-	set_text(str(game.poupanca)) #SETA O VALOR DA POUPANÇA
