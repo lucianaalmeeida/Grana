b8bd0a4bf1ec57c6004c0b2adf3147deaff5e8ce
Luciana Almeida <41968007+mlucianaap@users.noreply.github.com>
2021-03-29 13:56:52 -0300

    Delete sexo.gd

--- sexo.gd
+++
@@ -1,8 +0,0 @@
-extends Sprite
-
-func _ready():
-	#Verifica qual o sexo do personagem escolhido para carregar o avatar correto na cena dos níveis
-	if game.sexo == "F":
-		set_texture(load("res://sprites/menina-nivel.png"))
-	elif game.sexo == "M":
-		set_texture(load("res://sprites/menino-nivel.png"))
